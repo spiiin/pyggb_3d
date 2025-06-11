6743d3321cb8cddacebfffa09d65ab52ccef1fdc
spiiin <sanya.boyko@gmail.com>
2025-06-12 00:25:50 +0200

    Method for constructing a plane with point+plane, line+line, point+vector+vector, polygon

--- src/wrap-ggb/index.ts
+++ src/wrap-ggb/index.ts
@@ -17,6 +17,7 @@ import { register as registerDistance } from "./wrappings/distance";
 import { register as registerIntersect } from "./wrappings/intersect";
 import { register as registerZoom } from "./wrappings/zoom";
 import { register as registerNumberOfObjects } from "./wrappings/number-of-objects";
+import { register as registerPlane } from "./wrappings/plane";
 
 import { register as registerClearConsole } from "./app-ui/clear-console";
 
@@ -54,6 +55,7 @@ declare var Sk: SkulptApi;
   registerIntersect(mod, appApi);
   registerZoom(mod, appApi);
   registerNumberOfObjects(mod, appApi);
+  registerPlane(mod, appApi);
 
   registerClearConsole(mod, appApi);
 
--- src/wrap-ggb/wrappings/plane.ts
+++ src/wrap-ggb/wrappings/plane.ts
@@ -23,6 +23,26 @@ type SkGgbPlaneCtorSpec =
       p1: string;
       p2: string;
       p3: string;
+    }
+  | {
+      kind: "polygon";
+      polygon: string;
+    }
+  | {
+      kind: "point-parallel";
+      point: string;
+      plane: string;
+    }
+  | {
+      kind: "two-lines";
+      line1: string;
+      line2: string;
+    }
+  | {
+      kind: "point-vectors";
+      point: string;
+      vector1: string;
+      vector2: string;
     };
 
 export const register = (
@@ -45,6 +65,22 @@ export const register = (
           setLabelArgs([spec.p1, spec.p2, spec.p3]);
           break;
         }
+        case "polygon": {
+          setLabelArgs([spec.polygon]);
+          break;
+        }
+        case "point-parallel": {
+          setLabelArgs([spec.point, spec.plane]);
+          break;
+        }
+        case "two-lines": {
+          setLabelArgs([spec.line1, spec.line2]);
+          break;
+        }
+        case "point-vectors": {
+          setLabelArgs([spec.point, spec.vector1, spec.vector2]);
+          break;
+        }
         default:
           throw new Sk.builtin.TypeError(
             `bad Plane spec kind "${(spec as any).kind}"`
@@ -59,30 +95,80 @@ export const register = (
     slots: {
       tp$new(args: Array<SkObject>, kwargs: KeywordArgsArray) {
         const badArgsError = new Sk.builtin.TypeError(
-          "Plane() arguments must be (point1, point2, point3)"
+          "Plane() arguments must be one of:\n" +
+          "  (point1, point2, point3)\n" +
+          "  (polygon)\n" +
+          "  (point, plane)\n" +
+          "  (line1, line2)\n" +
+          "  (point, vector1, vector2)"
         );
 
         const make = (spec: SkGgbPlaneCtorSpec) =>
           withPropertiesFromNameValuePairs(new mod.Plane(spec), kwargs);
 
-        if (args.length !== 3) {
-          throw badArgsError;
-        }
-
-        // Check that all arguments are points
-        args.forEach((arg: SkObject, i: number) => {
-          if (!ggb.isGgbObjectOfType(arg, "point")) {
-            throw new Sk.builtin.TypeError(`point${i + 1} must be a point`);
+        switch (args.length) {
+          case 1: {
+            if (!ggb.isGgbObject(args[0])) {
+              throw badArgsError;
+            }
+            const objType = ggb.ggbType(args[0] as SkGgbObject);
+            if (objType === "polygon" || 
+                objType === "polygon3D" || 
+                objType === "quadrilateral" || 
+                objType === "pentagon" || 
+                objType === "hexagon" || 
+                objType === "triangle") {
+              return make({
+                kind: "polygon",
+                polygon: (args[0] as SkGgbObject).$ggbLabel,
+              });
+            }
+            throw badArgsError;
           }
-        });
-
-        const points = args as Array<SkGgbObject>;
-        return make({
-          kind: "three-points",
-          p1: points[0].$ggbLabel,
-          p2: points[1].$ggbLabel,
-          p3: points[2].$ggbLabel,
-        });
+          case 2: {
+            if (ggb.isGgbObjectOfType(args[0], "point") && 
+                ggb.isGgbObjectOfType(args[1], "plane")) {
+              return make({
+                kind: "point-parallel",
+                point: args[0].$ggbLabel,
+                plane: args[1].$ggbLabel,
+              });
+            }
+            if (ggb.isGgbObjectOfType(args[0], "line") && 
+                ggb.isGgbObjectOfType(args[1], "line")) {
+              return make({
+                kind: "two-lines",
+                line1: args[0].$ggbLabel,
+                line2: args[1].$ggbLabel,
+              });
+            }
+            throw badArgsError;
+          }
+          case 3: {
+            if (args.every(arg => ggb.isGgbObjectOfType(arg, "point"))) {
+              const points = args as Array<SkGgbObject>;
+              return make({
+                kind: "three-points",
+                p1: points[0].$ggbLabel,
+                p2: points[1].$ggbLabel,
+                p3: points[2].$ggbLabel,
+              });
+            }
+            if (ggb.isGgbObjectOfType(args[0], "point") && 
+                ggb.isGgbObjectOfType(args[1], "vector") && 
+                ggb.isGgbObjectOfType(args[2], "vector")) {
+              return make({
+                kind: "point-vectors",
+                point: args[0].$ggbLabel,
+                vector1: args[1].$ggbLabel,
+                vector2: args[2].$ggbLabel,
+              });
+            }
+            throw badArgsError;
+          }
+          default:
+            throw badArgsError;
+        }
       },
       tp$str(this: SkGgbPlane) {
         return new Sk.builtin.str(`Plane(${this.$ggbLabel})`);
